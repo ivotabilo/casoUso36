0fa992e33b6e76b84ab7e805acc7218bb0ddd592
Salocin0 <nicolasarieldurelli@gmail.com>
2022-06-30 20:09:26 -0300

    def fun

--- src/Model/GestorRegIngMant.java
+++ src/Model/GestorRegIngMant.java
@@ -1,10 +1,20 @@
-
 package Model;
 
+import java.util.Date;
+import java.util.List;
 
 public class GestorRegIngMant {
     private Usuario usuarioLog;
     private PersonalCientifico personalCientificoDeUsu;
+    private List<RecursoTecnologico> recursosTecnologicosDisponibles;
+    private List<RecursoTecnologico> recursosTecnologicosDisponiblesOrdenado;
+    private RecursoTecnologico SelRt;
+    private Date IngFecFin;
+    private Date IngRazMant;
+    private List<Turno> turnosRT;
+    private List<Turno> turnosRtOrdenado;
+    private String tipoNotificacion;
+    private Date fechaActual;
     
     public void nuevoIngMantCorre(){
         //to-do
@@ -18,21 +28,59 @@ public class GestorRegIngMant {
         return new PersonalCientifico();//to-do
     }
     
+    public List<RecursoTecnologico> buscarRtEnEstadoDisponible() {
+        return (List<RecursoTecnologico>) new RecursoTecnologico();//to-do
+    }
     
-    /*
-    buscarRtEnEstadoDisponible
-    ordenarPorTipoRT
-    tomarSelRt
-    tomarIngFecFin
-    tomarIngRazMant
-    buscarTurnosRT
-    ordenarTurnosPorCientifico
-    tomarConfirmacion
-    tomarSelFormaNotificacion
-    buscarEstadoCanceladoPorMantCorr
-    buscarEstadoConIngMantCorrectivo
-    getFechaHoraActua
-    crearMantenimiento
-    finCU
-    */
+    public List<RecursoTecnologico> ordenarPorTipoRT(List<RecursoTecnologico> recursosTecnologicosDisponibles){
+        //to-do
+        
+        return this.recursosTecnologicosDisponiblesOrdenado;
+    }
+    
+    public void tomarSelRt(RecursoTecnologico recursoTecnologico){
+        this.SelRt=recursoTecnologico;
+    }
+    
+    public void tomarIngFecFin(Date ingFecFin){
+        this.IngFecFin=ingFecFin;
+    }
+    
+    public void tomarIngRazMant(Date ingRazMant){
+        this.IngRazMant=ingRazMant;
+    }
+    
+    public List<Turno> buscarTurnosRT(){
+        return (List<Turno>) new Turno();//to-do
+    }
+    
+    public List<Turno> ordenarTurnosPorCientifico(List<Turno> turnosRT){
+        //to-do
+        
+        return this.turnosRtOrdenado;
+    }
+    
+    public void tomarConfirmacion(){
+        //to-do
+    }
+    
+    public void tomarSelFormaNotificacion(String tipoNotificacion){
+        this.tipoNotificacion=tipoNotificacion;  
+    }
+    
+    public Estado buscarEstadoCanceladoPorMantCorr(){
+        return new Estado();//to-do
+    }
+    
+    public Estado buscarEstadoConIngMantCorrectivo(){
+        return new Estado();//to-do
+    }
+    
+    public Date getFechaHoraActual(){
+        return new Date(); //to-do
+    }
+    
+    public void crearMantenimiento(){
+        //to-do
+    }
 }
